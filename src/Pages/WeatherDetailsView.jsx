83fd53572fee482cc74eaa336c9a163415c6272e
GDanushka <kpggayandanushka712@gmail.com>
2023-10-25 09:10:13 +0530

    staying

--- src/Pages/WeatherDetailsView.jsx
+++ src/Pages/WeatherDetailsView.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './WeatherDetailsView.css';
 
 function WeatherDetailsView() {
   const [data, setData] = useState({});
   const [forecastData, setForecastData] = useState([]);
-  const [location, setLocation] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
   const [showThreeDayForecast, setShowThreeDayForecast] = useState(false);
-  const weatherApiKey = '895284fb2d2c50a520ea537456963d9c';
+  const weatherApiKey = '895284fb2d2c50a520ea537456963d9c'; // Replace with your OpenWeatherMap API key
 
-  const fetchWeatherData = () => {
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${weatherApiKey}`;
+  const fetchWeatherData = (lat, lon) => {
+    // Validate latitude and longitude values
+    if (isNaN(lat) || isNaN(lon) || lat < -90 || lat > 90 || lon < -180 || lon > 180) {
+      console.error('Invalid latitude or longitude values.');
+      return;
+    }
+
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
 
     axios
       .get(weatherUrl)
       .then((response) => {
         setData(response.data);
+        setShowThreeDayForecast(false); // Reset to show weather details only
         if (response.data.coord) {
           const forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&exclude=current,minutely,hourly&units=imperial&appid=${weatherApiKey}`;
 
@@ -39,41 +46,16 @@ function WeatherDetailsView() {
     setShowThreeDayForecast(true);
   };
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      fetchWeatherData();
-      setLocation('');
-    }
-  };
-
-  const threeDays = (forecastData) => {
-    return (
-      <>
-        {forecastData.map((day, index) => (
-             <div key={ index } className="forecast-day">
-            <div
-              className='d-flex justify-content-center align-items-center mb-3'
-              style={ { backgroundColor: 'lightblue', borderRadius: '10px' } }>
-                 <p className="font-weight-bold">Date: { new Date(day.dt * 1000).toLocaleDateString() }</p>
-               </div>
-                 <div className='row text-center'>
-                    <div className='col-4'><p>Temperature</p></div>
-                    <div className='col-4'>:</div>
-                    <div className='col-4'><p> {day.temp.day}°F</p></div>
-                  </div>
-                  <div className='row text-center'>
-                    <div className='col-4'><p>Weather</p></div>
-                    <div className='col-4'>:</div>
-                    <div className='col-4'><p>{day.weather[0].main}</p></div>
-                  </div>
-           </div>
-        ))}
-      </>
-    );
+  const resetData = () => {
+    setData({});
+    setForecastData([]);
+    setShowThreeDayForecast(false);
+    setLatitude('');
+    setLongitude('');
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-1">
       <div className="row justify-content-center">
         <div className="col-md-4">
           <div className="card">
@@ -81,12 +63,18 @@ function WeatherDetailsView() {
               <h2 className="text-center mb-4">Weather Details</h2>
               <div className="mb-3">
                 <input
-                  value={location}
-                  onChange={(event) => setLocation(event.target.value)}
-                  onKeyPress={searchLocation}
-                  placeholder='Enter Location'
+                  value={latitude}
+                  onChange={(event) => setLatitude(event.target.value)}
+                  placeholder='Enter Latitude'
                   type="text"
                   className="form-control"
+                />
+                <input
+                  value={longitude}
+                  onChange={(event) => setLongitude(event.target.value)}
+                  placeholder='Enter Longitude'
+                  type="text"
+                  className="form-control mt-2"
                 />
               </div>
               {data.name && (
@@ -121,10 +109,41 @@ function WeatherDetailsView() {
               )}
               {data.name && !showThreeDayForecast && (
                 <div className="forecast">
-                  <button className='custom-button w-100' onClick={handleClick}>Show 3-Day Forecast</button>
+                  <button className='custom-button' onClick={handleClick}>Show 3-Day Forecast</button>
                 </div>
               )}
-              {showThreeDayForecast && threeDays(forecastData)}
+              {showThreeDayForecast && (
+                <div className="forecast-day">
+                  {forecastData.map((day, index) => (
+                    <div key={index}>
+                      <div
+              className='d-flex justify-content-center align-items-center'
+              style={ { backgroundColor: '#cdd3d39c', borderRadius: '5px' } }>
+                 <h5 className="font-weight-bold">Date: { new Date(day.dt * 1000).toLocaleDateString() }</h5>
+                      </div>
+                      <div className='row text-center'>
+                    <div className='col-4'><p>Temperature</p></div>
+                    <div className='col-4'>:</div>
+                    <div className='col-4'><p>{day.temp.day}°F</p></div>
+                      </div>
+                      <div className='row text-center'>
+                    <div className='col-4'><p>Feels Like</p></div>
+                    <div className='col-4'>:</div>
+                    <div className='col-4'><p>{data.main.feels_like.toFixed()}°C</p></div>
+                      </div>
+                      <div className='row text-center'>
+                    <div className='col-4'><p>Weather</p></div>
+                    <div className='col-4'>:</div>
+                    <div className='col-4'><p>{day.weather[0].main}</p></div>
+            </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+              <div className="forecast mt-3">
+                {!showThreeDayForecast && <button className="custom-button" onClick={() => fetchWeatherData(latitude, longitude)}>Search</button>}
+                {showThreeDayForecast && <button className="custom-button" onClick={resetData}>Reset</button>}
+              </div>
             </div>
           </div>
         </div>
@@ -134,3 +153,4 @@ function WeatherDetailsView() {
 }
 
 export default WeatherDetailsView;
+
